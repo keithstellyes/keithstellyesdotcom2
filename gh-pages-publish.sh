521eb6c4ca3e98d5f9a4ca06805ab1c3e612ddd7
Keith Stellyes <keithastellyes@gmail.com>
2025-05-06 12:44:43 -0700

    refactor to jinja

--- gh-pages-publish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git checkout main && \
-    git branch -d gh-pages && \
-    git checkout -b gh-pages && \
-    git push -f --set-upstream origin gh-pages
-git checkout main
--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
+function svgButton(svgName, text) {
+return `
+    <div>
+    <button onclick="alert('TODO: respond to ${text}')">
+        ${window.SVG[svgName]}
+        ${text}
+    </button>
+    </div>`
+}
+
 const INTRO_HTML = `<h1>Keith Stellyes' Page</h1>
 <p>This page as of May 6, 2025 is under construction, mind the dust :)</p>
 
-</hr>
-<div class="svgButton">
-${window.SVG.computer}
-<p>Technical interests!</p>
+<div class="svgButtonCollection">
+    ${svgButton("computer", "Technical interests!")}
+    ${svgButton("seedling", "How are my plants doing?")}
 </div>
-<div class="svgButton">
-${window.SVG.seedling}
-<p>How are my plants doing?!</p>
-</div>
-</hr>
 `;
 
-function onIndex() {
-    const contentElement = document.getElementById("content");
-    contentElement.innerHTML = INTRO_HTML;
-
-    const svgels = document.getElementsByTagName("svg");
-    for(const el of svgels) {
-        el.setAttribute("width", "100px");
-        el.setAttribute("height", "100px");
+function windowOnLoad() {
+    if (window.location.pathname.endsWith('index.html')) {
+        loadIndex();
+    } else {
+        console.log("Failed to determine page. Falling back to index.");
+        loadIndex();
     }
 }
 
+function loadIndex() {
+    console.log('Loading index page.');
+    const contentElement = document.getElementById("content");
+    contentElement.innerHTML = INTRO_HTML;
+}
+
 window.onload = () => {
-    onIndex();
+    console.log('path:', window.location.pathname);
+    windowOnLoad();
 }
