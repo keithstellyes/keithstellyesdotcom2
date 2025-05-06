a58aaaa540cde8ece20a3cd780dbab152d812e99
Keith Stellyes <keithastellyes@gmail.com>
2025-05-06 13:34:20 -0700

    plant details & history stuff

--- src/index.js
+++ src/index.js
@@ -1,35 +1,60 @@
-function svgButton(svgName, text) {
+function svgButton(svgName, text, onclick) {
 return `
     <div>
-    <button onclick="alert('TODO: respond to ${text}')">
+    <button onclick="${onclick}">
         ${window.SVG[svgName]}
         ${text}
     </button>
     </div>`
 }
 
-const INTRO_HTML = `<h1>Keith Stellyes' Page</h1>
-<p>This page as of May 6, 2025 is under construction, mind the dust :)</p>
-
+const INTRO_HTML = `
 <div class="svgButtonCollection">
-    ${svgButton("computer", "Technical interests!")}
-    ${svgButton("seedling", "How are my plants doing?")}
+    ${svgButton("computer", "Technical interests!", "alert('...')")}
+    ${svgButton("seedling", "How are my plants doing?", "loadPage('Plants')")}
 </div>
 `;
+PAGES = {
+    "Plants": {
+        html: "<p>Let's talk about my plants!</p><p>I have 3 columbines, a yard I'm working on, some young Japanese black pines, and more...</p>",
+        path: "plants"
+    },
+    "Index": {
+        html: INTRO_HTML,
+        path: "/"
+    }
+};
 
-function windowOnLoad() {
-    if (window.location.pathname.endsWith('index.html')) {
-        loadIndex();
-    } else {
-        console.log("Failed to determine page. Falling back to index.");
-        loadIndex();
+function loadPage(page, pushHistory=true) {
+    console.log("Loading the page:", page);
+    if(pushHistory) {
+        historyPush(page);
+    }
+    const contentElement = document.getElementById("content");
+    contentElement.innerHTML = PAGES[page].html;
+}
+
+
+function historyPush(page) {
+    try {
+        window.history.pushState({}, page, PAGES[page].path);
+    } catch(e) {
+        if (e.name === "SecurityError") {
+            console.log("Can't push to history:", e);
+        } else {
+            console.log("Not SecurityError, bubbling up...");
+            throw e;
+        }
     }
 }
 
-function loadIndex() {
-    console.log('Loading index page.');
-    const contentElement = document.getElementById("content");
-    contentElement.innerHTML = INTRO_HTML;
+function windowOnLoad() {
+    if (window.location.pathname.endsWith('index.html')) {
+        loadPage("Index", false);
+    } else {
+        console.log("Failed to determine page. Falling back to index.");
+        loadPage("Index", false);
+    }
 }
 
 window.onload = () => {
